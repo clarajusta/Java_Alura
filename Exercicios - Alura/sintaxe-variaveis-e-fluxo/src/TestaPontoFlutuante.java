cd0d56fefb14921c0fa65c0ea5f6eb7e50fbdaa8
Clara Justa <50603484+clarajusta@users.noreply.github.com>
2021-06-24 15:39:06 -0400

    Adicionando arquivos iniciais do curso de Java

---
+++ Exercicios - Alura/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,17 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("O meu salário é: " + salario);
+		
+		int divisao = 5/2;
+		System.out.println("Divisão com interio, 5/2, o valor é: " + divisao);
+		
+		double divisao2 = 5.0/2;
+		System.out.println("Divisão com ponto flutuante, 5.0/2, o valor é: " + divisao2);
+		
+	}
+}
---
+++ Exercicios - Alura/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		
+		int idade;
+		idade = 25;
+		
+		System.out.println("Olá novo teste");
+		
+		System.out.println("A minha idade é " + idade);
+	}
+}
