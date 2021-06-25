612b6c64761ea2f02dccb88116cc11c7bed8dad9
clarajusta <clarajusta31@gmail.com>
2021-06-25 13:23:58 -0400

    Uso da condicional if

---
+++ Exercicios - Alura/sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
@@ -0,0 +1,16 @@
+public class TestaCondicional {
+    public static void main(String[] args) {
+
+        System.out.println("Testando Condicionais");
+
+        int idade = 20;
+
+        if (idade>=18){
+            System.out.println("Maior de idade");
+        }
+
+        else
+        System.out.println("Menor de idade");
+
+    }
+}
