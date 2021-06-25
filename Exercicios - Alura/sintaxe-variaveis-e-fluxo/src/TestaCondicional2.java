c3503183761b5b74c6fa33e7df2444c47bf6e5be
clarajusta <clarajusta31@gmail.com>
2021-06-25 13:54:27 -0400

    uso de OU na condicional if

---
+++ Exercicios - Alura/sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,18 @@
+public class TestaCondicional2 {
+    public static void main(String[] args) {
+
+        System.out.println("Testando Condicionais");
+
+        int idade = 17;
+        int quantidadePessoas = 2;
+
+        if (idade>=18 || quantidadePessoas>1){
+            System.out.println("Maior de idade ou acompanhado");
+        }
+
+        else{
+                System.out.println("Menor de idade desacompanhado");
+        }
+
+    }
+}
